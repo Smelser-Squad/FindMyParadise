df5b325cdd0b915ff5ec7d0eeb7729e80dae1524
bdeyo28 <77344033+bdeyo28@users.noreply.github.com>
2021-03-23 16:33:25 -0400

    Host service controller (#26)
    * Added Host Model, Exceptions, and Repository
    
    * Added Service and Controller Layer To Host
    
    Co-authored-by: Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
    Co-authored-by: kpugh97 <55714544+kpugh97@users.noreply.github.com>

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/controllers/HostController.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/controllers/HostController.java
@@ -1,6 +1,10 @@
 package com.tp.wrc.findmyparadise.controllers;
 
 
+
+import com.tp.wrc.findmyparadise.exceptions.InvalidHostIDException;
+import com.tp.wrc.findmyparadise.exceptions.NullHostIDException;
+
 import com.tp.wrc.findmyparadise.models.Host;
 import com.tp.wrc.findmyparadise.services.HostService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +19,82 @@ public class HostController {
     @Autowired
     HostService service;
 
+
+    @PostMapping("/host/")
+    public ResponseEntity createHost(@RequestBody Host newHost)
+    {
+        try {
+            return ResponseEntity.ok(service.create(newHost));
+        }
+        catch (NullHostIDException | InvalidHostIDException ex)
+        {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("/host/{id}")
+    public ResponseEntity getHostByID(@PathVariable Integer hostID)
+    {
+        try {
+            return ResponseEntity.ok(service.show(hostID));
+        }
+        catch (NullHostIDException | InvalidHostIDException ex)
+        {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
+    @GetMapping("/hosts/")
+    public ResponseEntity getHosts()
+    {
+        try {
+            return ResponseEntity.ok(service.getAll());
+        }
+        catch (NullHostIDException | InvalidHostIDException ex)
+        {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
+    @PutMapping("/hosts/{id}")
+    public ResponseEntity editHostByID(@PathVariable Integer hostID, @RequestBody Host newHost)
+    {
+        try {
+            return ResponseEntity.ok(service.update(hostID, newHost));
+        }
+        catch (NullHostIDException | InvalidHostIDException ex)
+        {
+            return ResponseEntity.badRequest().body(ex.getMessage());
+        }
+    }
+
+    @DeleteMapping("/hosts/{id}")
+    public String deleteHostByID(@PathVariable Integer hostID)
+    {
+        String toReturn = "";
+
+        try {
+            if (service.destroy(hostID)) {
+                toReturn = "Event " + hostID + " deleted";
+            } else {
+                toReturn = "Event " + hostID + " not found";
+            }
+        }
+        catch (InvalidHostIDException | NullHostIDException ex)
+        {
+            ex.getMessage();
+        }
+
+        return toReturn;
+    }
+
+
 //    @PostMapping("/host/")
 //    public ResponseEntity createHost(@RequestBody Host newHost)
 //    {
 //
 //    }
+
 
 
 
--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/HostService.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/HostService.java
@@ -1,4 +1,25 @@
 package com.tp.wrc.findmyparadise.services;
 
+import com.tp.wrc.findmyparadise.exceptions.InvalidHostIDException;
+import com.tp.wrc.findmyparadise.exceptions.NullHostIDException;
+import com.tp.wrc.findmyparadise.models.Event;
+import com.tp.wrc.findmyparadise.models.Host;
+
+import java.util.List;
+
 public interface HostService {
+
+    public List<Host> getAll() throws InvalidHostIDException, NullHostIDException;
+
+    public Host show(Integer hostID) throws InvalidHostIDException, NullHostIDException;
+
+    public Host create(Host newHost) throws InvalidHostIDException, NullHostIDException;
+
+    public Host update(Integer hostID, Host newHost) throws InvalidHostIDException, NullHostIDException;
+
+    public boolean destroy(Integer hostID) throws InvalidHostIDException, NullHostIDException;
+
+
+public interface HostService {
+
 }
