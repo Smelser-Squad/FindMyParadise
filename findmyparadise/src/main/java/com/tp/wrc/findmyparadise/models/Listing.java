a781963b0d78c157f0e0aa7b6c40a104f84643cc
William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
2021-03-23 14:07:53 -0400

    Initial listing model created with getters and setters

---
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Listing.java
@@ -0,0 +1,116 @@
+package com.tp.wrc.findmyparadise.models;
+
+public class Listing {
+    private Integer listingID;
+    private String name;
+    private Double latitude;
+    private Double longitude;
+    private Integer hostID;
+    private String address;
+    private String description;
+    private Double price;
+    private Integer maxGuests;
+    private Double serviceFee;
+    private Double occupancyFee;
+    private Double cleaningFee;
+
+    public Listing() {
+
+    }
+
+    public Integer getListingID() {
+        return listingID;
+    }
+
+    public void setListingID(Integer listingID) {
+        this.listingID = listingID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getHostID() {
+        return hostID;
+    }
+
+    public void setHostID(Integer hostID) {
+        this.hostID = hostID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getMaxGuests() {
+        return maxGuests;
+    }
+
+    public void setMaxGuests(Integer maxGuests) {
+        this.maxGuests = maxGuests;
+    }
+
+    public Double getServiceFee() {
+        return serviceFee;
+    }
+
+    public void setServiceFee(Double serviceFee) {
+        this.serviceFee = serviceFee;
+    }
+
+    public Double getOccupancyFee() {
+        return occupancyFee;
+    }
+
+    public void setOccupancyFee(Double occupancyFee) {
+        this.occupancyFee = occupancyFee;
+    }
+
+    public Double getCleaningFee() {
+        return cleaningFee;
+    }
+
+    public void setCleaningFee(Double cleaningFee) {
+        this.cleaningFee = cleaningFee;
+    }
+}
