dc5dd0ada29cd18c734d49bedc7eca4bfd865088
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-03-23 09:39:16 -0400

    Added Spring Boot Project

---
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/FindmyparadiseApplication.java
@@ -0,0 +1,13 @@
+package com.tp.wrc.findmyparadise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindmyparadiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindmyparadiseApplication.class, args);
+	}
+
+}
