0c4e98057a3cdb5633a30ea4c650f9c53567a85c
Brendan Deyo <brendandeyo@C02DFXG3ML7H.local>
2021-03-23 16:25:05 -0400

    Added Service and Controller Layer To Host

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/HostServiceImpl.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/HostServiceImpl.java
@@ -1,4 +1,74 @@
 package com.tp.wrc.findmyparadise.services;
 
-public class HostServiceImpl {
+import com.tp.wrc.findmyparadise.exceptions.InvalidHostIDException;
+import com.tp.wrc.findmyparadise.exceptions.NullHostIDException;
+import com.tp.wrc.findmyparadise.models.Event;
+import com.tp.wrc.findmyparadise.models.Host;
+import com.tp.wrc.findmyparadise.repositories.HostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class HostServiceImpl implements HostService {
+
+    @Autowired
+    HostRepository hRepo;
+
+    @Override
+    public List<Host> getAll() throws InvalidHostIDException, NullHostIDException {
+        return hRepo.findAll();
+    }
+
+    @Override
+    public Host show(Integer hostID) throws InvalidHostIDException, NullHostIDException {
+        Host newHost = null;
+        Optional<Host> opt = hRepo.findById(hostID);
+        if (opt.isPresent()) {
+            newHost = opt.get();
+
+            return newHost;
+        }
+        return null;
+    }
+
+    @Override
+    public Host create(Host newHost) throws InvalidHostIDException, NullHostIDException {
+        return hRepo.saveAndFlush(newHost);
+    }
+
+    @Override
+    public Host update(Integer hostID, Host newHost) throws InvalidHostIDException, NullHostIDException {
+        Host hostToUpdate = hRepo.findById(hostID).get();
+
+        if (hostToUpdate != null) {
+
+            hostToUpdate.setHostID(hostID);
+            hostToUpdate.setHostName(newHost.getHostName());
+            hostToUpdate.setTotalReviews(newHost.getTotalReviews());
+            hostToUpdate.setSuperHost(newHost.isSuperHost());
+            hostToUpdate.setVerified(newHost.isVerified());
+            hostToUpdate.setResponseTime(newHost.getResponseTime());
+            hostToUpdate.setResponseRate(newHost.getResponseRate());
+            hostToUpdate.setJoinDate(newHost.getJoinDate());
+            hostToUpdate.setEmail(newHost.getEmail());
+            hostToUpdate.setImageSrc(newHost.getImageSrc());
+
+            return hRepo.saveAndFlush(hostToUpdate);
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean destroy(Integer hostID) throws InvalidHostIDException, NullHostIDException {
+        Host newHost = hRepo.findById(hostID).get();
+
+        if (newHost != null) {
+            hRepo.delete(newHost);
+            return true;
+        }
+
+        return false;
+    }
 }
