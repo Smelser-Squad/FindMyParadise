06234152e321a6314fb0fc62ecdd9c0547e7d590
Charles Hall <charleshall@C02DFWLKML7H.local>
2021-03-23 15:18:49 -0400

    Added @Column tags to remaining field variables.

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Amenity.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Amenity.java
@@ -13,7 +13,10 @@ public class Amenity implements Serializable {
     @Column(name = "amenity_id")
     private Integer amenity_id;
 
+    @Column(name = "amenity_name")
     private String amenity_name;
+
+    @Column(name = "amenity_category")
     private String amenity_category;
 
     //GETTERS & SETTERS
