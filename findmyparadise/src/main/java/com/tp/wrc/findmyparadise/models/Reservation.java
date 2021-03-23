039b3ced7f8727bcba74c49f1b6d1c8fcc418e42
Zach Miles <zachmiles@C02DQ7N1ML7H.local>
2021-03-23 15:05:23 -0400

    updated reservation model

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Reservation.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Reservation.java
@@ -1,34 +1,116 @@
 package com.tp.wrc.findmyparadise.models;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Entity
 @Table
-public class Reservation {
+public class Reservation implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "reservation_id")
     private Integer reservationId;
 
-    @Column
+    @Column(name = "check_in_date", nullable = false)
     private LocalDate checkInDate;
 
-    @Column
+    @Column(name = "check_out_date", nullable = false)
     private LocalDate checkOutDate;
 
-    @Column
+    @Column(name = "price", nullable = false)
     private Double price;
 
-    @Column
+    @Column(name = "adults", nullable = false)
     private Integer adults;
 
-    @Column
+    @Column(name = "children", nullable = false)
     private Integer children;
 
-    @Column
+    @Column(name = "infants", nullable = false)
     private Integer infants;
 
-    @OneToMany(mappedBy = "reservation", fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_listing")
     private Listing listing;
+
+    public Reservation() {}
+
+    public Reservation(Integer reservationId, LocalDate checkInDate, LocalDate checkOutDate, Double price, Integer adults, Integer children, Integer infants, Listing listing) {
+        this.reservationId = reservationId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.price = price;
+        this.adults = adults;
+        this.children = children;
+        this.infants = infants;
+        this.listing = listing;
+    }
+
+    public Integer getReservationId() {
+        return reservationId;
+    }
+
+    public void setReservationId(Integer reservationId) {
+        this.reservationId = reservationId;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getAdults() {
+        return adults;
+    }
+
+    public void setAdults(Integer adults) {
+        this.adults = adults;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    public Integer getInfants() {
+        return infants;
+    }
+
+    public void setInfants(Integer infants) {
+        this.infants = infants;
+    }
+
+    public Listing getListing() {
+        return listing;
+    }
+
+    public void setListing(Listing listing) {
+        this.listing = listing;
+    }
+
+
 }
