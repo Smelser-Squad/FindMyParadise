2d1c339e0bc701c238f3ecf5f17dec3dd2aaaaca
Stephen May <stephendouglasm@gmail.com>
2021-03-23 15:47:02 -0400

    more set up to Calendar model to implement JPA

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/CalendarService.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/CalendarService.java
@@ -6,10 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CalendarService {
+public interface CalendarService {
 
-    @Autowired
-    CalendarMySQLDao dao;
 
-    
+
+
 }
