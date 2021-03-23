09704754335064085454ef3de8742e93928b6750
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-03-23 11:27:01 -0400

    Initial skeletons for Event controller, service, dao interface, and dao impl.

---
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/controllers/EventController.java
@@ -0,0 +1,60 @@
+package com.tp.wrc.findmyparadise.controllers;
+
+
+import com.tp.wrc.findmyparadise.exceptions.InvalidEventIdException;
+import com.tp.wrc.findmyparadise.exceptions.NullEventIdException;
+import com.tp.wrc.findmyparadise.models.Event;
+import com.tp.wrc.findmyparadise.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+@CrossOrigin(origins = "http://localhost:4200")
+public class EventController {
+    @Autowired
+    EventService service;
+
+    //Adds a new event to the database by the given event model.
+    @PostMapping("/events/")
+    public Integer addEvent(@RequestBody Event event) {
+        return service.addEvent(event);
+    }
+
+    //Retrieves an event from the database by the given id.
+    @GetMapping("/events/{id}")
+    public Event getEventById(@PathVariable Integer id) throws InvalidEventIdException, NullEventIdException {
+        return service.getEventById(id);
+    }
+
+    //Retrieves a list of all Events in the database.
+    @GetMapping("/events/")
+    public List<Event> getEvents() {
+        return service.getEvents();
+    }
+
+    //Edits an existing Event in the database by replacing its attributes with the
+    //attributes of the given Event model.
+    @PutMapping("/events/{id}")
+    public String editEvent(@PathVariable Integer id, @RequestBody Event event) throws InvalidEventIdException {
+        if (service.editEvent(id, event)) {
+            return "Event " + id + " updated";
+        } else {
+            return "Event " + id + " not found";
+        }
+
+    }
+
+    //Deletes an existing Event from the database.
+    @DeleteMapping("/events/{id}")
+    public String deleteEvent(@PathVariable Integer id) throws NullEventIdException, InvalidEventIdException {
+        if (service.deleteEvent(id)) {
+            return "Event " + id + " deleted";
+        } else {
+            return "Event " + id + " not found";
+        }
+    }
+
+}
---
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/EventService.java
@@ -0,0 +1,55 @@
+package com.tp.wrc.findmyparadise.services;
+
+
+import com.tp.wrc.findmyparadise.exceptions.InvalidEventIdException;
+import com.tp.wrc.findmyparadise.exceptions.NullEventIdException;
+import com.tp.wrc.findmyparadise.models.Event;
+import com.tp.wrc.findmyparadise.persistence.EventPostgresDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EventService {
+
+    @Autowired
+    EventPostgresDao dao;
+
+    public Integer addEvent(Event event) {
+        return dao.addEvent(event);
+    }
+
+    public Event getEventById(Integer id) throws NullEventIdException, InvalidEventIdException {
+        if (id == null) {
+            throw new NullEventIdException("You cannot retrieve an Event with null id.");
+        }
+
+        Event event = dao.getEventById(id);
+        if (event == null) {
+            throw new InvalidEventIdException("No Event with that ID exists.");
+        } else {
+            return event;
+        }
+
+    }
+
+    //Retrieves a list of all existing Events in the database.
+    public List<Event> getEvents() {
+        return dao.getEvents();
+    }
+
+    public boolean editEvent(Integer id, Event event) throws InvalidEventIdException {
+        return dao.editEvent(id, event);
+    }
+
+    //Deletes an existing Event from the database.
+    //An exception will be thrown if given a null for the id to check for when deleting.
+    public boolean deleteEvent(Integer id) throws NullEventIdException, InvalidEventIdException {
+        if (id == null) {
+            throw new NullEventIdException("You cannot delete an Event with null id.");
+        }
+        return dao.deleteEvent(id);
+    }
+
+}
