ceb67e439683eca621ce4edcc29c2c6c17c201f9
Noel <53280725+noel-castillo@users.noreply.github.com>
2021-03-25 10:20:06 -0400

    Backend event (#71)
    * Syncing. No changes.
    
    * Setting up tests for event entity and services.
    
    Co-authored-by: noel-castillo <noelcastillo@C02DQ7WHML7H.local>

---
+++ findmyparadise/src/test/java/com/tp/wrc/findmyparadise/models/EventTest.java
@@ -0,0 +1,29 @@
+package com.tp.wrc.findmyparadise.services;
+
+import com.tp.wrc.findmyparadise.models.Event;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+class EventTest {
+
+	@Autowired
+	EventServiceImpl toTest;
+
+
+	//this will run before each @Test method
+	@BeforeEach
+	public void setup() {
+
+	}
+
+
+}
