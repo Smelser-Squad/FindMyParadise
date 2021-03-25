6b84b760308efaf3537e610fafc50765cdf0eba9
Stephen May <stephendouglasm@gmail.com>
2021-03-25 10:30:14 -0400

    Front End components in place and stylized

--- findmyparadise_frontend/src/App.vue
+++ findmyparadise_frontend/src/App.vue
@@ -1,22 +1,32 @@
 <template>
+<div id="AppBody">
   <div class="container">
     <reservation title="Reservation"></reservation>
   </div>
+  <div class="container">
+    <calendar title="Calendar"></calendar>
+  </div>
+  <div class="container">
   <Map />
+  </div>
+  <div class="scrollContainer">
   <more-places></more-places>
+  </div>
+  </div>
 </template>
 
 <script>
 import MorePlaces from "./components/MorePlaces";
 import Reservation from "./components/Reservation";
 import Map from "./components/Map";
+import Calendar from "./components/Calendar.vue"
 
 export default {
   name: "App",
   components: {
     Reservation,
     Map,
-
+    Calendar,
     MorePlaces,
   },
 };
@@ -72,5 +82,18 @@ body {
 .btn-block {
   display: block;
   width: 100%;
+}
+.scrollContainer {
+  max-width: 1200px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid gray;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
+}
+.datePicker {
+  display: inline;
 }
 </style>
