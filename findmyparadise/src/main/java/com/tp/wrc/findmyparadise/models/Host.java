563b0722b87ad3a1d70b10bfb5b96fbdc4beb929
Jason-Williams-github <jwilliams@talentpath.com>
2021-03-25 14:02:24 -0400

    commit for master pull

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Host.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Host.java
@@ -15,7 +15,7 @@ public class Host implements Serializable {
     // VARIABLES
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "host_id")
     private Integer hostID; // PK NOT NULL
 
