44ddec94ea50ebcd4e980610760f7ccf3ad0605d
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-03-25 13:14:16 -0400

    find by name method implementation

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/ListingService.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/services/ListingService.java
@@ -7,19 +7,11 @@ import java.util.List;
 
 public interface ListingService {
 
-//     List<Listing> index();
-//     Listing show(int id) throws NoListingFoundException;
-//     Listing create(Listing listing);
-//     boolean destroy(int id);
-////     Listing findByName(String name);
-////     List<Listing> findByHostID(Integer hostID);
-////     List<Listing> findByPrice(Double price);
-
      List<Listing> index();
      Listing show(Integer id) throws NoListingFoundException;
      Listing create(Listing listing);
      boolean destroy(Integer id);
-//     Listing findByNameIgnoreCase(String listingName) throws NoListingFoundException, InvalidListingNameException;
+     List<Listing> findByNameIgnoreCase(String listingName) throws NoListingFoundException, InvalidListingNameException;
 //     List<Listing> findByHostID(Integer hostID) throws NullHostIDException, InvalidHostIDException;
 //     List<Listing> findByPrice(Double price) throws NoListingFoundException;
 
