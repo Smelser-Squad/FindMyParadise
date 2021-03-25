eda458925cc70c2ab48e392eede29b6e3d86a581
wcollins37 <collinswilliam122396@gmail.com>
2021-03-25 10:44:39 -0400

    Can now add listings and retrieve them by id (#70)
    * Initial listing model created with getters and setters
    
    * Basic listing model, repository, and service layers started
    
    * Small typo fixed
    
    * Minor changes
    
    * Added reviews and reservations to listing model
    
    * Starting listing controller methods
    
    * Added model for single Photo
    
    * Small db changes
    
    * Removed merge artifacts
    
    Co-authored-by: William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
    Co-authored-by: kpugh97 <55714544+kpugh97@users.noreply.github.com>

---
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/controllers/ListingController.java
@@ -0,0 +1,35 @@
+package com.tp.wrc.findmyparadise.controllers;
+
+import com.tp.wrc.findmyparadise.exceptions.InvalidListingIDException;
+import com.tp.wrc.findmyparadise.exceptions.NoListingFoundException;
+import com.tp.wrc.findmyparadise.exceptions.NullListingIDException;
+import com.tp.wrc.findmyparadise.models.Listing;
+import com.tp.wrc.findmyparadise.services.ListingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ListingController {
+
+    @Autowired
+    ListingService service;
+
+    @PostMapping("/listing/")
+    public ResponseEntity createListing(@RequestBody Listing newListing) {
+        return ResponseEntity.ok(service.create(newListing));
+    }
+
+    @GetMapping("/listing/{listingID}")
+    public ResponseEntity getListingByID(@PathVariable Integer listingID) {
+        Listing toReturn = null;
+        try {
+            toReturn = service.show(listingID);
+        } catch (NoListingFoundException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
+        return ResponseEntity.ok(toReturn);
+    }
+}
