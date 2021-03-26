1c87301c65d81104e0e3f589badd9e7be930726c
Jason-Williams-github <jwilliams@talentpath.com>
2021-03-26 16:37:09 -0400

    more test stuff

--- findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/HostServiceImplTests.java
+++ findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/HostServiceImplTests.java
@@ -6,6 +6,7 @@ import com.tp.wrc.findmyparadise.models.Event;
 import com.tp.wrc.findmyparadise.models.Host;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
