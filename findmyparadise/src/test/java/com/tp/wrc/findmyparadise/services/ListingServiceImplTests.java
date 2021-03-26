07d603c16a6a619ff5a7f66eb320f467785c1bc7
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-03-26 14:59:57 -0400

    adding controller; testing setup

---
+++ findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/ListingServiceImplTests.java
@@ -0,0 +1,18 @@
+package com.tp.wrc.findmyparadise.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ListingServiceImplTests {
+
+    @Autowired
+    ListingServiceImpl test;
+
+    public void setup()
+    {
+
+    }
+
+
+}
