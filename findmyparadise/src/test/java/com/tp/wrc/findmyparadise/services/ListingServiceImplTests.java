5b19bd0dcd1ede74f0a0bdc7b2e07a1fffdfe8a5
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-03-29 12:38:14 -0400

    added exceptions changes to listing model

--- findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/ListingServiceImplTests.java
+++ findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/ListingServiceImplTests.java
@@ -1,5 +1,6 @@
 package com.tp.wrc.findmyparadise.services;
 
+import com.tp.wrc.findmyparadise.exceptions.*;
 import com.tp.wrc.findmyparadise.models.Host;
 import com.tp.wrc.findmyparadise.models.Listing;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,8 +17,72 @@ public class ListingServiceImplTests {
     @Autowired
     ListingServiceImpl test;
 
-    public void setup()
+    @Test
+    public void createListingTest() {
+        Listing listing = new Listing();
+        try {
+            listing = test.create(listing, 1);
+        }
+        catch (NullHostIDException | InvalidHostIDException e)
+        {
+            fail();
+        }
+    }
+
+//    @Test
+//    public void createListingTestNullListingID()
+//    {
+//        assertThrows(NullListingIDException.class,);
+//    }
+//
+//    @Test
+//    public void createListingTestNullHostID()
+//    {
+//        assertThrows(NullHostIDException.class, );
+//    }
+//
+//    @Test
+//    public void createListingTestNullListingName()
+//    {
+//        assertThrows(NoListingFoundException.class,);
+//    }
+//
+//    @Test
+//    public void createListingTestNullAddress()
+//    {
+//        assertThrows(NullAddressException.class,);
+//
+//    }
+
+    @Test
+    public void createListingTestNullPrice()
+    {
+//        assertThrows(NullListingPriceException.class,);
+    }
+
+    public void getAllListingsTest()
     {
 
     }
+
+    public void getListingTest()
+    {
+
+    }
+
+    public void getListingByNameTest()
+    {
+
+    }
+
+    public void getListingByTypeTest()
+    {
+
+    }
+
+    public void getListingsByHostIDTest()
+    {
+
+    }
+
 }
