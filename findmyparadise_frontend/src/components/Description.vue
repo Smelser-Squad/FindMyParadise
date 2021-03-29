5864242b87912ea79ad7b33ee9f062ef50b4aa5a
William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
2021-03-29 09:08:09 -0400

    Adding basic Vue file

---
+++ findmyparadise_frontend/src/components/Description.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="description">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Description"
+        data() {
+            return {
+                dataObject: {},
+            };
+        },
+        mounted() {
+            axios.get(`http://localhost:8080/api/listing/${listingID}`).then((res) => {
+            this.dataObject = res.data
+            console.log(res.data);
+    });
+        }
+    }
+</script>
