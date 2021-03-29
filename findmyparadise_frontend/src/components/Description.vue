c8368de7513f895b105ce034c8c8b13b10bea9d2
wcollins37 <collinswilliam122396@gmail.com>
2021-03-29 14:34:01 -0400

    All text elements in description box are present (#136)
    * Adding basic Vue file
    
    * Syncing
    
    * Hello World component displaying
    
    * Can retrieve real DB data and display on Description component
    
    * All text added to description
    
    Co-authored-by: William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>

--- findmyparadise_frontend/src/components/Description.vue
+++ findmyparadise_frontend/src/components/Description.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="description">
-        <h4>{{dataObject.type}} hosted by {{dataObject.host.hostName}}</h4>
+        <h4>{{dataObject.name}}</h4>
+        <span v-if="dataObject.reviews != undefined">{{dataObject.reviews.length}} reviews - <u>{{dataObject.address}}</u></span>
+        <h5 v-if="dataObject.host != undefined">{{dataObject.type}} hosted by {{dataObject.host.hostName}}</h5>
+        <span>{{dataObject.maxGuests}} guests - {{dataObject.bedrooms}} bedrooms - {{dataObject.bathrooms}} bathrooms</span>
+        <p>{{dataObject.description}}</p>
+        <span @click="showEmail()"><u>Contact Host</u></span>
     </div>
 </template>
 
@@ -13,13 +18,20 @@
         data() {
             return {
                 dataObject: {},
+                overlay: false
             };
         },
         mounted() {
             axios.get(`http://localhost:8080/api/listing/${listingID}`).then((res) => {
-            this.dataObject = res.data;
-            console.log(res.data);
-    });
+                this.dataObject = res.data;
+                console.log(res.data);
+            });
+        },
+        methods: {
+            showEmail() {
+                this.overlay = !this.overlay;
+                console.log(this.overlay);
+            }
         }
     }
 </script>
