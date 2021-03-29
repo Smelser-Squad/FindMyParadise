2b8e6361d1b9e58a136c5c581683fa3e5ff05e37
William Collins <williamcollins@C02DQ8A9ML7H.attlocal.net>
2021-03-29 09:54:07 -0400

    Hello World component displaying

--- findmyparadise_frontend/src/components/Description.vue
+++ findmyparadise_frontend/src/components/Description.vue
@@ -1,12 +1,15 @@
 <template>
     <div id="description">
-        
+        <span>Hello World!</span>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+
+    let listingID = 1;
     export default {
-        name: "Description"
+        name: "Description",
         data() {
             return {
                 dataObject: {},
@@ -14,9 +17,13 @@
         },
         mounted() {
             axios.get(`http://localhost:8080/api/listing/${listingID}`).then((res) => {
-            this.dataObject = res.data
+            this.dataObject = res.data;
             console.log(res.data);
     });
         }
     }
 </script>
+
+<style scoped>
+
+</style>
