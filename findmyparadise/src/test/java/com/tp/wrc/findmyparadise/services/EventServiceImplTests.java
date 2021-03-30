acf6a19ac151f56ed3316023a29ed111740d23fc
Jeffrey Mann <jmann@talentpath.com>
2021-03-30 13:19:50 -0400

    removed testng and h2 dependencies from pom.xml

--- findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/EventServiceImplTests.java
+++ findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/EventServiceImplTests.java
@@ -6,14 +6,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class EventServiceImplTests {
--- findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/ReviewServiceImplTests.java
+++ findmyparadise/src/test/java/com/tp/wrc/findmyparadise/services/ReviewServiceImplTests.java
@@ -12,7 +12,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.testng.Assert.fail;
 
 @SpringBootTest
 public class ReviewServiceImplTests {
