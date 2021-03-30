b7d1bde69e898f11fb443c4f84a317f702f98f21
Jason-Williams-github <jwilliams@talentpath.com>
2021-03-30 12:49:45 -0400

    front end stuff

---
+++ findmyparadise_frontend/src/components/Host.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>Host</h1>
+</template>
+
+<script>
+// import axios from "axios";
+
+// let hostId = 1;
+// export default {
+//     name: "Host",
+//     data(){
+//     }
+// }
+
+</script>
