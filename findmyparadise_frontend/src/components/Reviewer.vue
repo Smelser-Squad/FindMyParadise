70401ba41f685d3d2c8e7ad2eb1286264c107415
YesratRahman <49998106+YesratRahman@users.noreply.github.com>
2021-03-31 17:06:35 -0400

    Frontend review (#180)
    * Upload Image
    
    * File Uploader Controller added
    
    * Final Push / upload image not working

--- findmyparadise_frontend/src/components/Reviewer.vue
+++ findmyparadise_frontend/src/components/Reviewer.vue
@@ -12,73 +12,126 @@
       <input
         type="text"
         name="description"
-        placeholder="Write Rating Description"
+        placeholder="Write Rating  Description"
         v-model="posts.description"
       />
       <br />
       <br />
-      <input
+      <!-- <input
         type="text"
         name="imageSrc"
         placeholder="Upload Image"
         v-model="posts.imageSrc"
-      />
+      /> -->
+
+      <form ref="uploadForm" @submit.prevent="submit">
+        <input type="file" ref="uploadImage" @change="onImageUpload()" class="form-control" required>
+        <input type="button" @click="startUpload" name="upload" value="Upload" />
+      </form>
       <br />
       <br />
 
-      <date-picker
+      <!-- <date-picker
         class="datePicker"
         v-model="posts.joinedDate"
         lang="en"
         type="date"
         format="MM-dd-YYYY"
-      ></date-picker>
+      ></date-picker> -->
 
+      <br/> 
       <button type="submit">Add Review</button>
+      <br/> 
+      <br/> 
     </form>
   </div>
+ 
+  <h3>Review Details</h3>
+  <div v-for="reviewer in list" v-bind:key="reviewer.reviewId"> 
+    <p> Name: {{reviewer.name}}</p>
+    <p> Description: {{reviewer.description}}</p>
+    <p> Date: {{reviewer.joinedDate}} </p>
+
+  </div>
+ 
 </template>
 
 <script>
 import axios from "axios";
-import DatePicker from "vue3-datepicker";
 
 export default {
   name: "Review",
-  components: {
-    DatePicker,
-  },
+  
   data() {
     return {
       posts: {
         name: null,
         imageSrc: null,
         description: null,
-        joinedDate: null,
       },
+      list: undefined, 
+      formData : null, 
     };
   },
+ 
+// reviewData(){
+//     return {
+//       list: undefined
+//     }
+//   },
+
+//  fileUploadData: ()=> ({
+//     formData: null
+//   }),
+  
+
+
   methods: {
     postData(e) {
       axios
         .post("http://localhost:8080/api/addReviewer", this.posts)
         .then((result) => {
-          // console.log(result);
           console.warn(result);
         });
       e.preventDefault();
     },
+
+    onImageUpload(){
+    let file = this.$refs.uploadImage.files[0];
+    this.formData = new FormData(); 
+    this.formData.append("file", file); 
   },
 
-  // mounted() {
-  //   axios
-  //     .post("http://localhost:8080/api/addReviewer")
-  //     .then((response) => {
-  //       this.reviewers = console.log(response)
-  //       .finally(() => console.log('Data oading complete'));
+  startUpload(){
+    axios({
+      url: 'http://localhost:8080/api/upload',
+      method: 'POST',
+      data: this.formData,
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data'
+      },
+    }).then(response => {
+      console.log(response)
+    });
+    
 
-  //     })
-  //     .catch((err) => Promise.reject(err));
-  // },
+  }
+  },
+
+ 
+    mounted() {
+    axios
+      .get("http://localhost:8080/api/reviewers")
+      .then((res) => {
+        console.log(res.data);
+        this.list = res.data;
+      })
+      .catch((err) => Promise.reject(err));
+  }
+
+  
+  
+ 
 };
 </script>
