7f607ea3f09ad3dd5219315925ad6dfebddc8173
Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>
2021-04-01 15:13:37 -0400

    Updated

--- findmyparadise_frontend/src/App.vue
+++ findmyparadise_frontend/src/App.vue
@@ -1,55 +1,64 @@
 <template>
   <div id="AppBody">
+    <div class="masterContainer">
+      <div class="header">
+        <img src="" />
+      </div>
+      <div class="imgContainer">
+        <GalleryMini />
+      </div>
+      <div class="reservationContainer">
+       <Reservation
+        title="Reservation"
+        :dateStart="range.start"
+        :dateEnd="range.end"
+      />
+      </div>
+      <br />
+      <div class="amenitiesContainer">
+        <amenities title="Amenities"></amenities>
+      </div>
+      <div class="mapContainer">
+        <div class="left" style="width: 100%; height: 350px">
+          <h4 id="dateHeader">Select Date Range</h4>
+            <DateRangePicker @datePick="transferDates($event)" />
+        </div>
+        <div class="right">
+          <Map />
+        </div>
+      </div>
+      <div class="container">
+        <description title="Description" @email="toggleEmail"> </description>
+      </div>
+      <div class="reviewContainer">
+        <review></review>
+      </div>
+      <div class="reviewerContainer">
+        <reviewer> </reviewer>
+      </div>
 
-    <div class="header">
-      <h1 data-text="Find Your Paradise">Find Your Paradise</h1>
+      <div class="container">
+        <host></host>
+      </div>
+      <div class="scrollContainer">
+        <more-places></more-places>
+      </div>
+      <div class="scrollContainer">
+        <events title="Events"></events>
+      </div>
+      <email
+        v-if="emailTrigger && host != undefined"
+        :toggleEmail="() => toggleEmail()"
+        :host="host"
+      >
+      </email>
     </div>
-    <br>
-    <div class="reservationContainer">
+    </div>
+    
+  
+  
 
-    <div class="imgContainer">
-      <GalleryMini />
-    </div>
-    <div class="container">
 
-      <reservation title="Reservation"></reservation>
-    </div>
-    <div class="container">
-      <amenities title="Amenities"></amenities>
-    </div>
-    <div class="mapContainer">
-      <Map />
-    </div>
-    <div class="scrollContainer">
-      <more-places></more-places>
-    </div>
-    <div class="container">
-      <description title="Description" @email="toggleEmail"> </description>
-    </div>
-     <div class="container">
-      <host></host>
-    </div>
-    <div class="reviewContainer">
-      <review></review>
-    </div>
-    <div class="reviewerContainer">
-      <reviewer> </reviewer>
-    </div>
-
-    <div class="container">
-      <host></host>
-    </div>
-    <div class="scrollContainer">
-      <events title="Events"></events>
-    </div>
-    <email
-      v-if="emailTrigger && host != undefined"
-      :toggleEmail="() => toggleEmail()"
-      :host="host"
-    >
-    </email>
-  </div>
-  </div>
 </template>
 
 <script>
@@ -58,18 +67,15 @@ import Reservation from "./components/Reservation.vue";
 import Map from "./components/Map.vue";
 import Description from "./components/Description";
 import Amenities from "./components/Amenities.vue";
-
 import Review from "./components/Review";
 import Reviewer from "./components/Reviewer";
 import Events from "./components/Events";
+import DateRangePicker from "./components/DateRangePicker.vue";
 import Email from "./components/Email";
-
 
 import GalleryMini from "./components/GalleryMini.vue";
 
-
 import Host from "./components/Host.vue";
-
 
 export default {
   name: "App",
@@ -81,16 +87,12 @@ export default {
     Amenities,
     Review,
     Reviewer,
+    Host,
+    DateRangePicker,
     GalleryMini,
     Events,
     Email,
-    Host
   },
-
-
-   
-   
-  
 
   methods: {
     Change(event) {
@@ -100,13 +102,23 @@ export default {
       this.emailTrigger = !this.emailTrigger;
       this.host = host;
     },
-  },
 
+    transferDates(range) {
+      this.range = range;
+
+      console.log(this.range.start);
+      console.log(this.range.end);
+    },
+  },
 
   data() {
     return {
       emailTrigger: false,
       host: {},
+      range: {
+        start: new Date(),
+        end: new Date(),
+      },
     };
   },
 };
@@ -131,14 +143,15 @@ body {
   margin: auto;
   text-align: center;
   position: relative;
-  text-align: center;
-  margin-top: 30px;
-  margin-bottom: 45px;
   -webkit-box-reflect: -28px below linear-gradient(transparent, #0002);
+}
+.header img {
+  padding-bottom: 20px;
 }
 .header div {
   margin: auto;
   text-align: center;
+  margin-bottom: 20%;
 }
 .header h1 {
   font-size: 6vw;
@@ -148,8 +161,8 @@ body {
     #e2204c,
     #f04e39,
     #f67526,
-    #f39911,
-    #eabc12
+    rgb(252, 118, 140),
+    hotpink
   );
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
@@ -157,7 +170,7 @@ body {
 .header h1::after {
   content: attr(data-text);
   position: absolute;
-  margin-left: 4.5%;
+  margin-left: 20.5%;
   top: 0;
   left: 0;
   /* Change the position of transformed element */
@@ -171,8 +184,8 @@ body {
     #e2204c,
     #f04e39,
     #f67526,
-    #f39911,
-    #eabc12
+    rgb(252, 118, 140),
+    hotpink
   );
   -webkit-background-clip: text;
   color: transparent;
@@ -191,29 +204,41 @@ body {
   border-radius: 5px;
   box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
 }
-.reservationContainer {
-  max-width: 800px;
+.masterContainer {
+  max-width: 90%;
   margin: 30px auto;
   overflow: auto;
   min-height: 300px;
   border: 1px solid gray;
+  background: whitesmoke;
   padding: 30px;
   border-radius: 5px;
   box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
 }
-.reservationContainer div, h2, h5 {
+
+.masterContainer div,
+.reservationContainer div,
+h2,
+h5 {
   margin: auto;
   text-align: center;
 }
 .mapContainer {
-  max-width: 600px;
+  display: flex;
+  max-width: 80%;
   margin: 30px auto;
   overflow: auto;
   min-height: 300px;
-  border: 1px solid gray;
   padding: 30px;
+  /* border: 1px solid gray;
   border-radius: 5px;
-  box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
+  box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px; */
+}
+.mapContainer .left {
+  flex: 1;
+}
+.mapContainer .right {
+  flex: 1;
 }
 .mapContainer div {
   margin: auto;
@@ -221,6 +246,16 @@ body {
 }
 .mapContainer p {
   text-align: left;
+}
+.reservationContainer {
+  max-width: 95%;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid gray;
+  padding: 30px;
+  border-radius: 5px;
+  box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
 }
 .btn {
   display: inline-block;
@@ -261,7 +296,6 @@ body {
 .datePicker {
   display: inline;
 }
-
 .reviewContainer {
   max-width: 500px;
   margin: 30px auto;
@@ -272,7 +306,6 @@ body {
   border-radius: 5px;
   box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
 }
-
 .reviewerContainer {
   max-width: 500px;
   margin: 30px auto;
@@ -284,14 +317,29 @@ body {
   box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
 }
 
-.imgContainer{
+.imgContainer {
   max-width: 1300px;
+
   margin: 30px auto;
   object-fit: fill;
   max-height: 500px;
   border-radius: 10px;
   box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
-  overflow: hidden;
+  overflow: auto;
+}
+
+.amenitiesContainer {
+  max-width: 80%;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  padding: 30px;
+  /* border: 1px solid gray;
+  border-radius: 5px;
+  box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px; */
+}
+#dateHeader {
+  margin: auto;
+  text-align: center;
 }
 </style>
-
