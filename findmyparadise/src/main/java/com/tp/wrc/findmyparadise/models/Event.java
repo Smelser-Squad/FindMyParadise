c6f1ad2d5093025aced8433d5489d180a8ff2cac
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-04-02 14:32:18 -0400

    Implemented API route where user can retrieve a list of events within a given distance to a listing.

--- findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Event.java
+++ findmyparadise/src/main/java/com/tp/wrc/findmyparadise/models/Event.java
@@ -22,7 +22,7 @@ public class Event implements Serializable {
 
     @Column(name = "category")
     private String category;
-    
+
     @Column(name = "image_src")
     private String imageSrc;
 
