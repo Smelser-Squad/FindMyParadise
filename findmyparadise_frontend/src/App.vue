cf5dfe461b5365b2977014ac9383246216434f72
Kyara Pugh <kyarapugh@C02DQ8HSML7H.local>
2021-04-03 19:21:24 -0400

    adding information component

--- findmyparadise_frontend/src/App.vue
+++ findmyparadise_frontend/src/App.vue
@@ -50,6 +50,9 @@
       <div class="scrollContainer">
         <events title="Events"></events>
       </div>
+      <div class="scrollContainer">
+        <information></information>
+      </div>
       <email
         v-if="emailTrigger && host != undefined"
         :toggleEmail="() => toggleEmail()"
@@ -72,6 +75,8 @@ import DateRangePicker from "./components/DateRangePicker.vue";
 import Email from "./components/Email";
 import GalleryMini from "./components/GalleryMini.vue";
 import Host from "./components/Host.vue";
+import Information from "./components/Information.vue";
+
 export default {
   name: "App",
   components: {
@@ -86,6 +91,7 @@ export default {
     GalleryMini,
     Events,
     Email,
+    Information,
   },
   methods: {
     Change(event) {
