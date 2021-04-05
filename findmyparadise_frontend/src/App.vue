743ecbb6147705dc1a4c78e5ec753bad24184701
Tamera Brown <tamerabrown642@gmail.com>
2021-04-05 10:50:11 -0400

    Reservation component (#213)
    * Able to send data to backend
    
    * Able to add a reservation with a listing
    
    * Buttons are working for guests
    
    Co-authored-by: Tamera Brown <tamerabrown@C02DFWHUML7H.fios-router.home>

--- findmyparadise_frontend/src/App.vue
+++ findmyparadise_frontend/src/App.vue
@@ -12,6 +12,7 @@
           title="Reservation"
           :dateStart="range.start"
           :dateEnd="range.end"
+          :days="days"
         />
       </div>
       <hr class="light" />
@@ -22,7 +23,7 @@
       <hr class="light" />
       <div class="calenderContainer">
         <h4 id="dateHeader">Select Date Range</h4>
-        <DateRangePicker @datePick="transferDates($event)" />
+        <DateRangePicker @datePick="transferDates($event)" @diffDays="transferDays($event)" />
       </div>
       <hr class="light" />
       <div class="mapContainer">
@@ -344,15 +345,7 @@ h5 {
 }
 .descContainer {
   max-width: 500px;
-  margin: 30px auto;
-  overflow: auto;
-  min-height: 300px;
-  border: 1px solid gray;
-  padding: 30px;
-  border-radius: 5px;
-  box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px;
-  float: right;
-} */
+}
 .imgContainer {
   max-width: 100%;
   margin: 30px auto;
@@ -371,15 +364,6 @@ h5 {
   /* border: 1px solid gray;
   border-radius: 5px;
   box-shadow: rgb(0 0 0 / 12%) 0px 6px 16px; */
-}
-
-.reviewContainer{
-  max-width: 80%;
-  margin: 30px auto;
-  overflow: auto;
-  min-height: 300px;
-  padding: 30px;
-}
 #dateHeader {
   margin: auto;
   text-align: center;
