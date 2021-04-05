ea69459fc4aa5dca74abc5493c805d02b38da8ae
StephenDMay <71042502+StephenDMay@users.noreply.github.com>
2021-04-05 16:13:31 -0400

    Reservation calendar (#228)
    * generate calendar dates
    
    * generate start and end dates
    
    * added number of days calculator
    
    * Delete package-lock.json
    
    * Date range picker applied
    
    * semi passed dates to reservation
    
    * added infant/child button functionality
    
    * Update Listing.java
    
    * merge fix
    
    * added disabled dates
    
    * added disabling of reserved dates
    
    * fixed conflicts
    
    * package.lock fix
    
    * package.lock fix again
    
    * package.lock.... again.
    
    Co-authored-by: kpugh97 <55714544+kpugh97@users.noreply.github.com>
    Co-authored-by: Tamera Brown <tamerabrown642@gmail.com>

--- findmyparadise_frontend/src/components/Reservation.vue
+++ findmyparadise_frontend/src/components/Reservation.vue
@@ -10,10 +10,8 @@
     </header>
     <body id="ReservationBody">
       <form v-on:submit.prevent="submitForm" method="post">
-
       <h4>CheckIn:</h4><input disabled v-model="form.checkInDate"/>
        <h4>CheckOut:</h4><input disabled v-model="form.checkOutDate" />
-
      
         <h5>Guests:</h5>
         <Guests @iAQty="increaseAQty($event)" @dAQty="decreaseAQty($event)" 
@@ -23,12 +21,10 @@
         <br/>
         <p> {{maxGuests}} guests maximum, infants don't count towards number of guests</p>
         <span style="display:none;"> {{updateDate(dateEnd)}}</span>
-
        <span>{{updateDays(days)}}</span>
       
       
       
-
 
         <span
           class="_19di23v"
@@ -50,12 +46,9 @@
       <p style="color: gray; text-align: center">You won't be charged yet</p>
       
         <div>
-
           <u>${{ dailyPrice }} x {{ NumOfDays }} nights</u>
           <span>${{ dailyPrice * NumOfDays }}</span>
-
         </div>
-
         <div class="popup" @click="CleaningFeepopup()">
           <u>Cleaning Fee</u> <span> ${{ cleaningFee }}</span>
           <span class="popuptext" id="CleaningFeepopup"
@@ -92,7 +85,7 @@
 import Guests from "./Guests";
 import axios from "axios";
 import moment from "moment";
-let listingID = 1;
+let listingID = 2;
 export default {
   name: "Reservation",
   props: ["dateStart", "dateEnd", "days"],
@@ -150,8 +143,7 @@ export default {
       popup.classList.toggle("show");
     },
     submitForm() {
-      axios
-        .post(`http://localhost:8080/api/reservation/${listingID}`, this.form)
+      axios.post(`http://localhost:8080/api/reservation/${listingID}`, this.form)
         .then((res) => {
           console.log(res.data);
         });
