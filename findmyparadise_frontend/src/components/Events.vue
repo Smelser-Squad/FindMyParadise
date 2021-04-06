5419e476479dbcd0f3829d2d83961267bab05518
noel-castillo <noelcastillo@C02DQ7WHML7H.local>
2021-04-06 11:56:28 -0400

    Fixed route for events.

--- findmyparadise_frontend/src/components/Events.vue
+++ findmyparadise_frontend/src/components/Events.vue
@@ -6,8 +6,34 @@
       <template v-slot:default="{ item }">
         <div>
           <a target="_blank" href="">
-             <div class="image-container">
+            <div class="image-container">
               <img :src="item.imageSrc" />
+              <div class="image-overlay">
+                <div class="button-container" v-on:click.stop="saved(item.id)">
+                  <button>
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      aria-hidden="true"
+                      role="presentation"
+                      focusable="false"
+                      style="
+                        display: block;
+                        fill: rgba(0, 0, 0, 0.5);
+                        height: 24px;
+                        width: 24px;
+                        stroke: rgb(255, 255, 255);
+                        stroke-width: 2;
+                        overflow: visible;
+                      "
+                    >
+                      <path
+                        d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </div>
             </div>
 
             <div class="subtitle">
@@ -65,11 +91,16 @@ export default {
   },
   mounted() {
     axios
-      .get("http://localhost:8080/api/events/15/25")
+      .get(`http://54.91.69.145:80/api/events/${this.$route.props.listingId}/25`)
       .then((res) => {
         this.items = res.data;
       })
       .catch((err) => Promise.reject(err));
+  },
+  methods: {
+    saved(id) {
+      alert(`You saved listing with id: ${id}!`);
+    },
   },
 };
 </script>
