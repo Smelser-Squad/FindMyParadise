687b475d0750f036301945f6d72db2f2bc26c36d
VictorBarronG <77344068+VictorBarronG@users.noreply.github.com>
2021-04-06 09:17:20 -0400

    Gall impl (#232)
    * fixing styling
    
    * styling
    
    * changed get back to the deployed backend
    
    Co-authored-by: Victor Barron <victorbarron@C02DK48MML7H.attlocal.net>

--- findmyparadise_frontend/src/components/GalleryMini.vue
+++ findmyparadise_frontend/src/components/GalleryMini.vue
@@ -14,23 +14,15 @@
 
     <Modal v-if="popupTriggers.buttonTrigger" class="modal" 
                :TogglePopup="() => TogglePopup('buttonTrigger')">
-               <div class="insider">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" >
-                <div class="carousel-inner" >
-                    <div class="carousel-item active" v-for="(image,idx) in images" :key="image" :class="{ active: idx==0 }">
-                        <img :src="image.imageSrc"  class="d-block w-100" alt="" :class="{ active: idx==0 }">
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-            </div>
-            </div>
+             <div >
+                 <ul>
+                     <li><img :src="firstImage" class="sectioned"></li>
+                     <li><img :src="secondImage" class="sectioned"></li>
+                     <li><img :src="thirdImage" class="sectioned"></li>
+                     <li><img :src="fourthImage" class="sectioned"></li>
+                     <li><img :src="fifthImage" class="sectioned"></li>
+                 </ul>
+             </div>
     </Modal>
 </div>
 </template>
@@ -82,7 +74,7 @@ export default {
 
         })
     },
-    methods(){
+    methods: {
 
     }
 };
@@ -145,29 +137,13 @@ export default {
         object-fit: fill;
         overflow: hidden;
     }
-    .carousel-item{
-        object-fit: fill;
-    }
-    .carousel{
-        margin: 15px;
-        overflow: hidden;
-        object-fit: cover;
-        
-    }
-    .modal{
-        overflow: hidden;
 
-
+    .sectioned{
+        width: 95%;
+        margin: 10px;
+        margin-left: 4%;
     }
 
-    .contained{
-        overflow: hidden;
-    }
-
-    .insider{
-        height: 100%;
-        width: 100%;
-    }
     
     
 
